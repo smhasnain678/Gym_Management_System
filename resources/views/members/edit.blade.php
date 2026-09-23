396b3e5c906bf701f794975bf83d866fd02c4de0
Muhammad Hasnain <smhasnain43@gmail.com>
2026-09-23 19:58:43 +0500

    Changes

--- resources/views/members/edit.blade.php
+++ resources/views/members/edit.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
-@section('title', __('Edit Member'))
-@section('meta_description', __('Update member profile information.'))
-@section('page_title', __('Edit Member'))
+
 
 @section('content')
 <div class="max-w-4xl mx-auto space-y-6">
